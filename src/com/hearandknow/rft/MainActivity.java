4861ecd8eba174f1f1f92d8649e77a4cb20d1354
courdi95 <courdi95@gmail.com>
2014-02-28 14:30:53 +0100

    gps

--- src/com/hearandknow/rft/MainActivity.java
+++ src/com/hearandknow/rft/MainActivity.java
@@ -424,4 +424,87 @@ public class MainActivity extends Activity
 		return chaine_status;
 	}
 
+	
+	/////////////
+//	Button btnShowLocation;
+//
+//	     
+//
+//	    // GPSTracker class
+//
+//	    GPSTracker gps;
+//
+//	     
+//
+//	    @Override
+//
+//	    public void onCreate(Bundle savedInstanceState) {
+//
+//		        super.onCreate(savedInstanceState);
+//
+//		        setContentView(R.layout.main);
+//
+//		         
+//
+//		        btnShowLocation = (Button) findViewById(R.id.btnShowLocation);
+//
+//		         
+//
+//		        // show location button click event
+//
+//		        btnShowLocation.setOnClickListener(new View.OnClickListener() {
+//
+//				             
+//
+//				            @Override
+//
+//				            public void onClick(View arg0) {        
+//
+//					                // create class object
+//
+//					                gps = new GPSTracker(AndroidGPSTrackingActivity.this);
+//
+//					 
+//
+//					                // check if GPS enabled     
+//
+//					                if(gps.canGetLocation()){
+//
+//						                     
+//
+//						                    double latitude = gps.getLatitude();
+//
+//						                    double longitude = gps.getLongitude();
+//
+//						                     
+//
+//						                    // \n is for new line
+//
+//						                    Toast.makeText(getApplicationContext(), "Your Location is - \nLat: " + latitude + "\nLong: " + longitude, Toast.LENGTH_LONG).show();    
+//
+//					                }else{
+//
+//						                    // can't get location
+//
+//						                    // GPS or Network is not enabled
+//
+//						                    // Ask user to enable GPS/network in settings
+//
+//						                    gps.showSettingsAlert();
+//
+//					                }
+//
+//					                 
+//
+//				            }
+//
+//			        });
+//
+//	    }
+//
+	/////////
+	
+	
+	
+	
 }
